88dfc0d6f1d54aac6db7626bffe6e102ae7bca07
Wojtazz <marcin.witas72@gmail.com>
2024-08-24 20:46:23 +0200

    chore(web): install and setup tanstack vue query

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,10 +3,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from '@/router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueQueryPlugin)
 app.use(router)
 
 app.mount('#app')
