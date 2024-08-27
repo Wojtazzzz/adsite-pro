a91b82fca9853c6df4f9c10c425d73dae0fecf5a
Wojtazz <marcin.witas72@gmail.com>
2024-08-27 21:52:37 +0200

    feat(web): add switching theme button

--- apps/web/src/components/header/Header.vue
+++ apps/web/src/components/header/Header.vue
@@ -3,26 +3,32 @@ import Logo from '@/components/ui/Logo.vue';
 import DesktopNav from '@/components/header/DesktopNav.vue';
 import MobileNavDialog from '@/components/header/mobileNav/MobileNavDialog.vue';
 import Container from '@/components/ui/Container.vue';
+import ToggleTheme from '@/components/ui/ToggleTheme.vue';
 </script>
 
 <template>
-	<div class="border-b fixed top-0 left-0 z-30 bg-white w-full">
+	<div class="border-b fixed top-0 left-0 z-30 background w-full">
 		<Container>
 			<header class="relative flex items-center justify-between">
 				<RouterLink to="/" aria-label="Home page" class="inline-flex items-center">
 					<Logo />
 
-					<span class="ml-2.5 text-xl font-bold tracking-wide text-gray-800 uppercase">
+					<span
+						class="ml-2.5 text-xl font-bold tracking-wide text-primary-background uppercase"
+					>
 						Teamer
 					</span>
 				</RouterLink>
 
-				<div class="hidden md:block">
-					<DesktopNav />
-				</div>
+				<div class="flex gap-x-5">
+					<div class="hidden md:block">
+						<DesktopNav />
+					</div>
 
-				<div class="md:hidden">
-					<MobileNavDialog />
+					<div class="md:hidden">
+						<MobileNavDialog />
+					</div>
+					<ToggleTheme />
 				</div>
 			</header>
 		</Container>
