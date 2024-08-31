2559cc5ec8a95ba23ec4aa66c5241c0b3a6209e6
Wojtazz <marcin.witas72@gmail.com>
2024-08-31 18:12:00 +0200

    feat(web): add Textarea component

---
+++ apps/web/src/components/ui/Textarea.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Textarea } from '@/components/ui-library/textarea';
+import {
+	FormItem,
+	FormLabel,
+	FormField,
+	FormControl,
+	FormMessage,
+} from '@/components/ui-library/form';
+
+defineProps<{
+	name: string;
+	label: string;
+	placeholder?: string;
+}>();
+</script>
+
+<template>
+	<FormField v-slot="{ componentField }" name="bio">
+		<FormItem>
+			<FormLabel>
+				<span class="cursor-pointer font-semibold">
+					{{ label }}
+				</span>
+			</FormLabel>
+			<FormControl>
+				<Textarea :placeholder="placeholder" class="resize-none" v-bind="componentField" />
+			</FormControl>
+			<FormMessage />
+		</FormItem>
+	</FormField>
+</template>
