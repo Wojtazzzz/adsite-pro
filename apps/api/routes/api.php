ee87b01a9de10c8f7c115d2a0a051736fc621f43
Wojtazz <marcin.witas72@gmail.com>
2024-09-01 16:32:34 +0200

    refactor(api): move auth routes to api.php

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\Api\Controllers\AuthenticatedSessionController;
+use Modules\Auth\Api\Controllers\RegisteredUserController;
 use Modules\Task\Api\Controllers\CategoryController;
 use Modules\Task\Api\Controllers\InvitationController;
 use Modules\Task\Api\Controllers\TaskController;
 use Modules\Task\Api\Controllers\TeamController;
 use Modules\Task\Api\Controllers\UserController;
 
-require_once __DIR__ . '/auth.php';
+//require_once __DIR__ . '/auth.php';
 
 Route::middleware('auth')->name('.api')->group(function () {
     Route::controller(TeamController::class)->prefix('/teams')->name('.teams')->group(function () {
@@ -37,6 +39,19 @@ Route::middleware('auth')->name('.api')->group(function () {
 
     Route::controller(TaskController::class)->prefix('/tasks')->name('.tasks')->group(function () {
         Route::patch('/{task}/status', 'updateStatus')->name('.update.status');
+    });
+});
+
+
+Route::name('auth')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::post('/register', [RegisteredUserController::class, 'store'])->name('.register');
+        Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('.login');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/me', [AuthenticatedSessionController::class, 'me'])->name('.me');
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('.logout');
     });
 });
 
