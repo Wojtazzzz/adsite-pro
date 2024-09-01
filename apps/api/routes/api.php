239ff36312cd161686d9ee629d363373145ee166
Wojtazz <marcin.witas72@gmail.com>
2024-09-01 16:39:00 +0200

    refactor(api): fix routes names

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -9,49 +9,46 @@ use Modules\Task\Api\Controllers\TaskController;
 use Modules\Task\Api\Controllers\TeamController;
 use Modules\Task\Api\Controllers\UserController;
 
-//require_once __DIR__ . '/auth.php';
+Route::name('api.')->group(function () {
+    Route::name('auth.')->group(function () {
+        Route::middleware('guest')->group(function () {
+            Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+            Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+        });
 
-Route::middleware('auth')->name('.api')->group(function () {
-    Route::controller(TeamController::class)->prefix('/teams')->name('.teams')->group(function () {
-        Route::get('/', 'index')->name('.index');
-        Route::post('/', 'store')->name('.store');
-        Route::get('/{team}', 'show')->name('.show');
-        Route::delete('/{team}', 'destroy')->name('.delete');
-        Route::patch('/{team}/rename', 'rename')->name('.rename');
+        Route::middleware('auth')->group(function () {
+            Route::get('/me', [AuthenticatedSessionController::class, 'me'])->name('me');
+            Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+        });
+    });
 
-        Route::controller(CategoryController::class)->prefix('/{team}/categories')->name('.categories')->group(function () {
-            Route::post('/', 'store')->name('.store');
+    Route::controller(TeamController::class)->prefix('/teams')->middleware('auth')->name('teams.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{team}', 'show')->name('show');
+        Route::delete('/{team}', 'destroy')->name('delete');
+        Route::patch('/{team}/rename', 'rename')->name('rename');
 
-            Route::controller(TaskController::class)->prefix('/{category}/tasks')->name('.tasks')->group(function () {
-                Route::post('/', 'store')->name('.store');
+        Route::controller(CategoryController::class)->prefix('/{team}/categories')->name('categories.')->group(function () {
+            Route::post('/', 'store')->name('store');
+
+            Route::controller(TaskController::class)->prefix('/{category}/tasks')->name('tasks.')->group(function () {
+                Route::post('/', 'store')->name('store');
             });
         });
 
-        Route::controller(UserController::class)->prefix('/{team}/users')->name('.users')->group(function () {
-            Route::get('/', 'members')->name('.index');
-            Route::get('/details', 'details')->name('.details');
+        Route::controller(UserController::class)->prefix('/{team}/users')->name('users.')->group(function () {
+            Route::get('/', 'members')->name('index');
+            Route::get('/details', 'details')->name('details');
         });
 
-        Route::controller(InvitationController::class)->prefix('/{team}/invitations')->name('.invitations')->group(function () {
-            Route::post('/', 'store')->name('.store');
+        Route::controller(InvitationController::class)->prefix('/{team}/invitations')->name('invitations')->group(function () {
+            Route::post('/', 'store')->name('store');
         });
     });
 
-    Route::controller(TaskController::class)->prefix('/tasks')->name('.tasks')->group(function () {
-        Route::patch('/{task}/status', 'updateStatus')->name('.update.status');
-    });
-});
-
-
-Route::name('auth')->group(function () {
-    Route::middleware('guest')->group(function () {
-        Route::post('/register', [RegisteredUserController::class, 'store'])->name('.register');
-        Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('.login');
-    });
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/me', [AuthenticatedSessionController::class, 'me'])->name('.me');
-        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('.logout');
+    Route::controller(TaskController::class)->prefix('/tasks')->name('tasks')->group(function () {
+        Route::patch('/{task}/status', 'updateStatus')->name('update.status');
     });
 });
 
