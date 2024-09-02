793e0403075a3daaa236a520c81a7c5d40ecc31a
Wojtazz <marcin.witas72@gmail.com>
2024-09-02 19:04:44 +0200

    refactor(web): remove deleted pinia function

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,13 +1,11 @@
 import '@/assets/style.css';
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from '@/router';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(VueQueryPlugin, {
 	queryClientConfig: {
 		defaultOptions: {
